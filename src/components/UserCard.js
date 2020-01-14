904b4d8536c57fe753b9b69859b5bb82f5653dc3
CAM603 <cameronhawley@comcast.net>
2020-01-14 11:40:39 -0500

    added usercard

---
+++ src/components/UserCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserCard = (props) => {
+    console.log(props.data)
+    return (
+        <div>
+            <img style={{height: '150px'}} src={props.data.avatar_url}/>
+            <p>Name: {props.data.name}</p>
+            <p>Bio: {props.data.bio}</p>
+            <p>Location: {props.data.location}</p>
+            <p>Joined: {props.data.created_at}</p>
+            <p>Followers: {props.data.followers}</p>
+            <p>Following: {props.data.following}</p>
+            <p>Public Repositories: {props.data.public_repos}</p>
+        </div>
+    )
+}
+
+export default UserCard
